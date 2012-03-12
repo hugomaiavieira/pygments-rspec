dc1ec27655359def581f5b74d953ee813557d2df
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2012-03-12 02:13:42 -0300

    Go to version 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygments-rspec',
-    version='0.1',
+    version='0.2',
     description='Pygments lexer for Ruby + RSpec.',
     long_description=open('README.rst').read(),
     keywords='pygments ruby rspec lexer',
